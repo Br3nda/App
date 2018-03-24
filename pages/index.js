d30566ab4e2c4cdecbdd140316eee2d89d2765b6
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2018-03-24 23:51:35 +1300

    remove all remnants of firebase

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
 import Layouts from '../components/Layouts'
 import Head from 'next/head'
-import firebase from '../firebase'
-
-console.log('firebase name is ', firebase.name)
-const messaging = firebase.messaging()
-messaging.requestPermission()
-.then(function () {
-  console.log('Have permission')
-})
-.catch(function (err) {
-  console.log('Error happened')
-})
 
 const Index = () => (
   <div>
