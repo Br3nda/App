dfc19c8caf65902a4297ef834c54775d5bcdf5f2
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2018-03-25 00:35:41 +1300

    super simple ask for notifications permission on index page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
+import React from 'react'
+
 import Layouts from '../components/Layouts'
 import Head from 'next/head'
 
-const Index = () => (
-  <div>
-    <Head>
-      <link rel='manifest' href='static/manifest.json' />
-    </Head>
-    <Layouts>
-      <p>Welcome to our awesome app</p>
-    </Layouts>
-  </div>
-)
+export default class Index extends React.Component {
+  componentDidMount () {
+    Notification.requestPermission().then(function(result) {
+      console.log(result)
+    })
+  }
 
-export default Index
+  render () {
+    return (
+      <div>
+        <Head>
+          <link rel='manifest' href='static/manifest.json' />
+        </Head>
+        <Layouts>
+          <p>Welcome to our awesome app</p>
+        </Layouts>
+      </div>
+    )
+  }
+}
