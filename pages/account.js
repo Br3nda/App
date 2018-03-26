3371092e60c8ca6f713c164e2d7253270ba801ba
MeghanHughes <meghan@manu.net.nz>
2018-03-27 10:55:21 +1300

    removed <head>

--- pages/account.js
+++ pages/account.js
@@ -1,12 +1,8 @@
 import Layouts from '../components/Layouts'
 import Dashboard from '../components/Dashboard'
-import Head from 'next/head'
 
 const Account = () => (
   <div>
-    <Head>
-      <link rel='manifest' href ='static/manifest.json' />
-    </Head>
     <Layouts>
       <Dashboard />
     </Layouts>
