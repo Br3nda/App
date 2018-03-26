fa4b0bbebe179648fc2933d8829035ae8cb90658
Janie Tito <jtito@hotmail.com>
2018-03-26 22:00:37 +1300

    Add global styles to Layouts.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
 import Layouts from '../components/Layouts'
 import Head from 'next/head'
-import firebase from '../firebase'
-
-console.log('firebase name is ', firebase.name)
-const messaging = firebase.messaging()
-messaging.requestPermission()
-.then(function () {
-  console.log('Have permission')
-})
-.catch(function (err) {
-  console.log('Error happened')
-})
 
 const Index = () => (
   <div>
-    <Head><link rel='manifest' href='static/manifest.json' /> </Head>
-    <Layouts>
-      <p>Welcome to our awesome app</p>
-    </Layouts>
+    <Head>
+      <link rel='manifest' href ='static/manifest.json' />
+    </Head>
+    <Layouts />
   </div>
 )
 
