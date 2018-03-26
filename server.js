ff08dde49c4fac2c424e7f17c83bb6dc757378e2
MeghanHughes <meghan@manu.net.nz>
2018-03-27 12:25:07 +1300

    sets up express server. routes to index.js

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
-const { createServer } = require('http')
-const { join } = require('path')
-const { parse } = require('url')
-const { createReadStream } = require('fs')
+const express = require('express')
+const bodyParser = require('body-parser')
+const compression = require('compression')
 const next = require('next')
+const webpush = require("web-push")
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-  .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url, true)
-      const { pathname } = parsedUrl
-      if (pathname === '/service-worker.js') {
-        // const filePath = join(__dirname, '.next', pathname)
-        //
-        // app.serveStatic(req, res, filePath)
-        res.setHeader('content-type', 'text/javascript')
-        createReadStream('./service-worker.js').pipe(res)
-      } else {
-        handle(req, res, parsedUrl)
-      }
-    })
-    .listen(3000, () => {
-      console.log(`> Ready on http://localhost:${3000}`)
-    })
-  })
+const port = process.env.PORT || 3000
+
+const URL_MAP = {
+  '/index': '/index',
+};
+
+app.prepare().then(() => {
+  const server = express();
+
+  // give all Nextjs's request to Nextjs before anything else
+  server.get('/_next/*', (req, res) => {
+    handle(req, res);
+  });
+
+  // GET method route
+  server.get('*', (req, res) => {
+    const url = URL_MAP[req.path];
+    if (url) {
+      app.render(req, res, url);
+    } else {
+      handle(req, res);
+    }
+  });
+
+  server.get('/static/*', (req, res) => {
+    handle(req, res);
+  });
+
+  server.use(compression());
+  server.use(bodyParser.json());
+
+  server.listen(port)
+})
