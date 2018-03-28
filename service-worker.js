e0d96354b91a8813141bea4ae94ae84fd71b3fed
MeghanHughes <meghan@manu.net.nz>
2018-03-29 11:33:21 +1300

    edited notification

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,28 @@
-"use strict";
+'use strict'
 
-self.addEventListener("push", function(event) {
-  console.log("[Service Worker] Push Received.");
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.')
+  console.log(`[Service Worker] Push had this data: '${event.data.text()}'`)
 
-  const title = "Main Bedroom is too cold";
+  const title = 'Whare Alert'
   const options = {
     body:
-      "Try closing any windows or turn on heating. \nUse a dehumidifier if you have one.",
-    icon: "./static/img/icon/house.png",
-    badge: "./static/img/icon/house.png"
-  };
+      `Janie's room is TOO HOT. \nTry closing any windows or turn on heating. \nUse a dehumidifier if you have one.`,
+    icon: './static/img/icon/house.png',
+    badge: './static/img/icon/house.png'
+  }
 
   const notificationPromise = self.registration.showNotification(
     title,
     options
-  );
-  event.waitUntil(notificationPromise);
-});
+  )
+  event.waitUntil(notificationPromise)
+})
 
-self.addEventListener("notificationclick", function(event) {
-  console.log("[Service Worker] Notification click Received.");
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.')
 
-  event.notification.close();
+  event.notification.close()
 
-  event.waitUntil(clients.openWindow("localhost:3000/account"));
-});
+  event.waitUntil(clients.openWindow('localhost:3000/account'))
+})
