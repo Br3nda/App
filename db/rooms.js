99eff000b0c4f9b5f1b161e64933c25a48d0afc1
AMV <amv@AMVs-MacBook-Air.local>
2018-04-07 17:14:01 +1200

    add rooms.js where all the rooms data functions will live

---
+++ db/rooms.js
@@ -0,0 +1,15 @@
+// const seed = require('./seed.json')
+
+function checkHumidity (data) {
+  console.log(data)
+  return data
+}
+
+function humudityOutOfBounds (data) {
+  return 0
+}
+
+module.exports = {
+  checkHumidity,
+  humudityOutOfBounds
+}
