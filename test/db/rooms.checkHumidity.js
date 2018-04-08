7814fd195fcbe9f91147876b1e0a35a9e2d3a395
anna-marie-vos <amvos@outlook.com>
2018-04-08 17:01:07 +1200

    clean out expected array

--- test/db/rooms.checkHumidity.js
+++ test/db/rooms.checkHumidity.js
@@ -1,22 +1,11 @@
 import test from 'ava'
 import rooms from '../../db/rooms'
 
-
 test(`test 2: if any room humidity is to high,
   it returns a warning message`, t => {
   const testData = require('../../db/seed.json')
 
   const expected = [
-    { 'id': '1',
-      'name': `Sandra's desk`,
-      'sensorType': 'humidity',
-      'warningMsg': `The humidity in Sandra's desk is too high!!`
-    },
-    { 'id': '2',
-      'name': `Janie's room`,
-      'sensorType': 'humidity',
-      'errorMsg': `The humidity in Janie's room is too high!!`
-    }
   ]
 
   const value = rooms.checkHumidity(testData)
