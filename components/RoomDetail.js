e053ff8e8e2cbfe0bc5a0431185fa8be17fd50be
MeghanHughes <meghan@manu.net.nz>
2018-04-09 15:24:38 +1200

    adds card summary component

--- components/RoomDetail.js
+++ components/RoomDetail.js
@@ -2,6 +2,8 @@ import Menu from './Menu'
 import Header from './Header'
 import ReportCard from './ReportCard'
 import HealthAlert from './HealthAlert'
+import Readings from './Readings'
+import SensorInfo from './SensorInfo'
 
 export default (props) => (
   <div>
@@ -12,39 +14,8 @@ export default (props) => (
         <h1 className='sm narrow-down-only'><a className='back' href='#'><img className='svg' src='./img/icon/back.svg' alt='back' /></a>Lisefski House</h1>
         <ReportCard />
         <HealthAlert />
-        <section className='card'>
-          <header className='condensed'>
-            <h2>Readings</h2>
-            <h4>Past 30 days</h4>
-            <ul className='legend'>
-              <li><span className='temp'></span>Temperature</li>
-              <li><span className='hum'></span>Humidity</li>
-            </ul>
-          </header>
-          <div className='graph'>
-            <div className='scroller'>
-              <img className='placeholder' src='./img/placeholder-graph.png' />
-            </div>
-          </div>
-        </section>
-        <section className='card'>
-          <header>
-            <h2>Sensor<span className='sensor'>146</span></h2>
-          </header>
-          <dl className='col4 restrict-width'>
-            <dt>First Detected</dt>
-            <dd>04 Jul, 2017</dd>
-            <dt>Last reading</dt>
-            <dd>5 minutes ago</dd>
-            <dt>Battery</dt>
-            <dd>76%</dd>
-            <dt>Software</dt>
-            <dd>v2.1</dd>
-          </dl>
-          <div className='right-action'>
-            <a className='btn small' href='#'>Disconnect from room</a>
-          </div>
-        </section>
+        <Readings />
+        <SensorInfo />
       </div>
     </div>
   </div>
