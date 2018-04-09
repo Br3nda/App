6a561a205f98dfc07ce7bf683a4f3cd621808e77
MeghanHughes <meghan@manu.net.nz>
2018-04-10 09:54:14 +1200

    adds edit and add whare modal components

--- components/profile/setUp/EditWhareModal.js
+++ components/profile/setUp/EditWhareModal.js
@@ -1,18 +1,18 @@
 const AddWhareModal = () => (
-  <div id='new-whare' class='modal mfp-hide'>
+  <div id='edit-whare' class='modal mfp-hide'>
     <header>
-      <h2>New Whare</h2>
+      <h2>Edit Whare</h2>
       <a class='head-action close' href='#'><img class='svg' src='./img/icon/x.svg' alt='close' /></a>
     </header>
     <ul class='form-row'>
       <li>
         <label>House Name</label>
-        <input type='text' />
+        <input type='text' value='Lisefski House' />
       </li>
       <li>
         <label>Property Type</label>
         <ul class='form-controls'>
-          <li><label><input type='radio' name='property_type' value='Owner Occupier' />Owner Occupier</label></li>
+          <li><label><input type='radio' name='property_type' value='Owner Occupier' checked />Owner Occupier</label></li>
           <li><label><input type='radio' name='property_type' value='Social Housing' />Social Housing</label></li>
           <li><label><input type='radio' name='property_type' value='Private Rental' />Private Rental</label></li>
           <li><label><input type='radio' name='property_type' value='Workplace / Office' />Workplace / Office</label></li>
@@ -21,6 +21,7 @@ const AddWhareModal = () => (
     </ul>
     <footer>
       <a class='btn submit' href='#'>Save Whare</a>
+      <a class='del' href='#'><img class='svg' src='./img/icon/del.svg' alt='icon' />Remove Whare</a>
     </footer>
   </div>
 )
