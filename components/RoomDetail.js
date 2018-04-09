1773a4fb038055d91c7717d52ef682eea314a8ab
MeghanHughes <meghan@manu.net.nz>
2018-04-09 14:49:26 +1200

    adds health alert componen

--- components/RoomDetail.js
+++ components/RoomDetail.js
@@ -1,6 +1,7 @@
 import Menu from './Menu'
 import Header from './Header'
 import ReportCard from './ReportCard'
+import HealthAlert from './HealthAlert'
 
 export default (props) => (
   <div>
@@ -10,18 +11,7 @@ export default (props) => (
       <div className='center'>
         <h1 className='sm narrow-down-only'><a className='back' href='#'><img className='svg' src='./img/icon/back.svg' alt='back' /></a>Lisefski House</h1>
         <ReportCard />
-        <section className='card alert health-alert with-image'>
-
-          <header>
-          <h2 className='secondary'>Health Warning</h2>
-          <a className='head-action' href='#'><img className='svg' src='./img/icon/x.svg' alt='close' /></a>
-          </header>
-          <p>The World Health Organisation recommends temperatures above 20 degrees for children. Allowing children to continue to sleep in this room without adequate heating may result in increased risk of respiratory illness. </p>
-          <a className='btn' href='#'><img className='svg' src='./img/icon/btn-i.svg' alt='icon' />Learn how to fix this</a>
-          <footer>
-            <p><a className='underline' href='#'>Dismiss alert</a> – I’m happy with the temperature of this room</p>
-          </footer>
-        </section>
+        <HealthAlert />
         <section className='card'>
           <header className='condensed'>
             <h2>Readings</h2>
