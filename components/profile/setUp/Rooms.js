26dbb3c091754ae59584748514539a6d59cb7478
MeghanHughes <meghan@manu.net.nz>
2018-04-09 22:33:45 +1200

    added RoomList component

--- components/profile/setUp/Rooms.js
+++ components/profile/setUp/Rooms.js
@@ -1,8 +1,12 @@
 import Header from '../../genApp/Header'
+import Menu from '../../genApp/Menu'
+import RoomsList from './RoomsList'
 
 const Rooms = () => (
   <div>
     <Header />
+    <Menu />
+    <RoomsList />
   </div>
 )
 
