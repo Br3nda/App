44e5297eae699d6743478294874a127427f1a816
Janie Tito <jtito@hotmail.com>
2018-04-11 22:11:54 +1200

    Fix style for login page on desktop and mobile

--- components/genApp/Login.js
+++ components/genApp/Login.js
@@ -1,41 +1,52 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Login = () => (
-  <div className='content'>
-    <div className='center'>
-      <header className='header'>
-        <a className='logo' href='./'>Whare Hauora</a>
-        <h1 className='lg'>Kia ora</h1>
-        <h4 className='lg'>Login to start using Whare Hauora</h4>
+  <div className="content">
+    <div className="center">
+      <header className="header">
+        <a className="logo" href="./">
+          Whare Hauora
+        </a>
+        <h1 className="lg">Kia ora</h1>
+        <h4 className="lg">Login to start using Whare Hauora</h4>
       </header>
-      <section className='card pre-log'>
-        <div className='img'></div>
-        <div className='text'>
-          <h2 className='secondary'>Login</h2>
-          <ul className='form-row'>
+      <section className="card pre-log">
+        <div className="img" />
+        <div className="text">
+          <h2 className="secondary">Login</h2>
+          <ul className="form-row">
             <li>
               <label>Email Address</label>
-              <input type='text' />
+              <input type="text" />
             </li>
             <li>
               <label>Password</label>
-              <input type='password' />
+              <input type="password" />
             </li>
             <li>
-              {' '}
-              <Link href='/dashboard' replace>
-                <a className='btn submit'>Login</a>
-              </Link>{' '}
+              {" "}
+              <Link href="/dashboard" replace>
+                <a className="btn submit">Login</a>
+              </Link>{" "}
             </li>
           </ul>
           <footer>
-            <p className='left'><a href='./forgot-password.html' className='underline'>Forgot password?</a></p>
-            <p className='right'>Don’t have an account? <a href='./sign-up.html' className='underline'>Sign up</a></p>
+            <p className="left">
+              <a href="./forgot-password.html" className="underline">
+                Forgot password?
+              </a>
+            </p>
+            <p className="right">
+              Don’t have an account?{" "}
+              <a href="./sign-up.html" className="underline">
+                Sign up
+              </a>
+            </p>
           </footer>
         </div>
       </section>
     </div>
   </div>
-)
+);
 
-export default Login
+export default Login;
