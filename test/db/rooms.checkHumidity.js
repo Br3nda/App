3508ee4b1e8846a0f1c2d6deb5c8439318ee9a5e
anna-marie-vos <amvos@outlook.com>
2018-04-13 13:54:02 +1200

    adding exampleData.json

--- test/db/rooms.checkHumidity.js
+++ test/db/rooms.checkHumidity.js
@@ -5,8 +5,41 @@ test(`test 2: if any room humidity is to high,
   it returns a warning message`, t => {
   const testData = require('../../db/seed.json')
 
-  const expected = [
-  ]
+  const expected = {
+    '6': {
+      'name': `Sandra's desk`,
+      'roomUse': 'office',
+      'updated-at': '2018-04-06T02:04:45.394Z',
+      'grade': 'B',
+      'generalMsg': ``,
+      'alertMsg': ``,
+      'temperature': {
+        'value': 23.5,
+        'unit': '°C',
+        'timestamp': '2018-04-06 02:04:45 UTC',
+        'errorMsg': '',
+        'checklistMsg': '',
+        'tooLow': true,
+        'tooHigh': false
+      },
+      'humidity': {
+        'value': 63.0,
+        'unit': '%',
+        'timestamp': '2018-04-06 02:04:45 UTC',
+        'errorMsg': '',
+        'checklistMsg': '',
+        'tooHigh': true
+      },
+      'dewpoint': {
+        'value': 12.5,
+        'unit': '°C',
+        'timestamp': '2018-04-06 02:04:45 UTC',
+        'errorMsg': '',
+        'checklistMsg': '',
+        'tooHigh': false
+      }
+    }
+  }
 
   const value = rooms.checkHumidity(testData)
   // rooms.checkHumidity()
