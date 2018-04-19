eaa39c0cb93b3aefc7fa903891528ae38888cdfe
MeghanHughes <meghan@manu.net.nz>
2018-04-19 16:17:01 +1200

    renders AddRoom onClick

--- components/profile/setUp/AddRoomModal.js
+++ components/profile/setUp/AddRoomModal.js
@@ -4,8 +4,40 @@ class AddRoomModal extends React.Component {
 
   render () {
     return(
-      <div>
-      add a damn room yo
+      <div id='new-room' >
+      <h2>New Room</h2>
+      <a className='head-action close' href='#'><img className='svg' src='./img/icon/x.svg' alt='close' /></a>
+      <ul className='form-row'>
+      <li>
+      <label>Room Name</label>
+      <input type='text' />
+      </li>
+      <li>
+      <label>Room use</label>
+      <ul className='form-controls'>
+      <li><label><input type='radio' name='room_use' value='Living Space' />Living Space</label></li>
+      <li><label><input type='radio' name='room_use' value='Bedroom / Sleeping' />Bedroom / Sleeping</label></li>
+      <li><label><input type='radio' name='room_use' value='Kid’s Bedroom' />Kid’s Bedroom</label></li>
+      <li><label><input type='radio' name='room_use' value='Baby’s Bedroom' />Baby’s Bedroom</label></li>
+      </ul>
+      </li>
+      </ul>
+      <div className='inset'>
+      <h3>Sensor</h3>
+      <ul className='form-row'>
+      <li>
+      <label>Room use</label>
+      <ul className='form-controls'>
+      <li><label><input type='radio' name='sensor' value='253' /><span className='sensor'>253</span>First detected about 1 month ago</label></li>
+      <li><label><input type='radio' name='sensor' value='45' /><span className='sensor'>45</span>First detected 15 days ago</label></li>
+      <li><label><input type='radio' name='sensor' value='Later' />Pair with sensor later</label></li>
+      </ul>
+      </li>
+      </ul>
+      </div>
+      <footer>
+      <a className='btn submit' href='#'>Save Room</a>
+      </footer>
       </div>
     )
   }
@@ -14,40 +46,3 @@ class AddRoomModal extends React.Component {
 export default AddRoomModal
 
       // <a classNameName='btn-add append-table open-modal' href='#' data-mfp-src='#new-room'><img classNameName='svg' src='../../../static/img/icon/btn-add.svg' alt='icon' />Add room</a>
-
-
-      // <div id='new-room' className='modal mfp-hide'>
-      // <h2>New Room</h2>
-      // <a className='head-action close' href='#'><img className='svg' src='./img/icon/x.svg' alt='close' /></a>
-      // <ul className='form-row'>
-      // <li>
-      // <label>Room Name</label>
-      // <input type='text' />
-      // </li>
-      // <li>
-      // <label>Room use</label>
-      // <ul className='form-controls'>
-      // <li><label><input type='radio' name='room_use' value='Living Space' />Living Space</label></li>
-      // <li><label><input type='radio' name='room_use' value='Bedroom / Sleeping' />Bedroom / Sleeping</label></li>
-      // <li><label><input type='radio' name='room_use' value='Kid’s Bedroom' />Kid’s Bedroom</label></li>
-      // <li><label><input type='radio' name='room_use' value='Baby’s Bedroom' />Baby’s Bedroom</label></li>
-      // </ul>
-      // </li>
-      // </ul>
-      // <div className='inset'>
-      // <h3>Sensor</h3>
-      // <ul className='form-row'>
-      // <li>
-      // <label>Room use</label>
-      // <ul className='form-controls'>
-      // <li><label><input type='radio' name='sensor' value='253' /><span className='sensor'>253</span>First detected about 1 month ago</label></li>
-      // <li><label><input type='radio' name='sensor' value='45' /><span className='sensor'>45</span>First detected 15 days ago</label></li>
-      // <li><label><input type='radio' name='sensor' value='Later' />Pair with sensor later</label></li>
-      // </ul>
-      // </li>
-      // </ul>
-      // </div>
-      // <footer>
-      // <a className='btn submit' href='#'>Save Room</a>
-      // </footer>
-      // </div>
